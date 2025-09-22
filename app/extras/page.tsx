23f7a903aee2130a3a7b0603733aa8aef0fb46ec
Noah <noah.stewart.colleges@gmail.com>
2025-09-22 14:45:39 -0400

    Updated extras page description

--- app/extras/page.tsx
+++ app/extras/page.tsx
@@ -1,3 +1,3 @@
 export default function Notes() {
-    return <div className="prose"><h1>Extras</h1><p>Coming soon.</p></div>;
+    return <div className="prose"><h1>Extras</h1><p>This page will contain information about my hobbies, current readings, and notes. I am working on it now!</p></div>;
 }
